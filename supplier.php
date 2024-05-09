29617a94d00923865cf39eaf99c42ec580285dea
Erych Qwin Rubio <eqcrubio2022@plm.edu.ph>
2024-05-09 14:00:35 +0800

    Update supplier.php

--- supplier.php
+++ supplier.php
@@ -6,8 +6,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="includes/main.css">
-    <link rel="stylesheet" href="includes/supplier.css">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/supplier.css">
     <title>Manage Group</title>
 </head>
 
